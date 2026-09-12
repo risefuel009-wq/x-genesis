ead6cd1e84391508b4dfe0a4bfcd7f4c4b7f2fc9
risefuel009-wq <risefuel009@gmail.com>
2026-09-12 21:21:44 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compress: true,
-  poweredByHeader: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
